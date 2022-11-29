7f65cf5a07126d54a26381ff2b982de4b61935f3
KiX7777 <kristianboi202@gmail.com>
2022-11-29 18:49:26 +0100

    design finished; added bbasic functions; when = is pressed MINUS function runs; first number is set by the user while the second is fixed at 2

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 'use strict';
 
+//selektori
+const display = document.querySelector('.display');
+const broj = document.querySelectorAll('.broj');
+let userTyping = false;
+
 //računske operacije - BASIC
 
 //ZBRAJANJE
 
 const zbroj = function (broj1, broj2) {
-  broj1 = +prompt('Insert number');
-  broj2 = +prompt('Insert number');
+  broj1 = +prompt('Insert number').replace(',', '.');
+  broj2 = +prompt('Insert number').replace(',', '.');
   return broj1 + broj2;
 };
 
+// const minus = function (broj1, broj2) {
+//   broj1 = +prompt('Insert number').replace(',', '.');
+//   broj2 = +prompt('Insert number').replace(',', '.');
+//   return broj1 - broj2;
+// };
+
 const minus = function (broj1, broj2) {
-  broj1 = +prompt('Insert number');
-  broj2 = +prompt('Insert number');
+  broj1 = spojeniBrojevi;
+  broj2 = 2;
   return broj1 - broj2;
 };
 const množ = function (broj1, broj2) {
-  broj1 = +prompt('Insert number');
-  broj2 = +prompt('Insert number');
+  broj1 = +prompt('Insert number').replace(',', '.');
+  broj2 = +prompt('Insert number').replace(',', '.');
   return broj1 * broj2;
 };
 const dijeljenje = function (broj1, broj2) {
-  broj1 = +prompt('Insert number');
-  broj2 = +prompt('Insert number');
+  broj1 = +prompt('Insert number').replace(',', '.');
+  broj2 = +prompt('Insert number').replace(',', '.');
   return broj1 / broj2;
 };
 
@@ -32,17 +43,80 @@ const operate = function (operator) {
 
   switch (operator.toLowerCase()) {
     case '+':
-      console.log(zbroj());
+      // console.log(zbroj());
+      display.textContent = zbroj();
       break;
     case '-':
-      console.log(minus());
+      display.textContent = minus();
       break;
     case 'x':
     case '*':
-      console.log(množ());
+      display.textContent = množ();
+
       break;
     case '/':
-      console.log(dijeljenje());
+      display.textContent = dijeljenje();
       break;
   }
 };
+
+//pritisak broja registrira broj na displayu
+broj.forEach((num) => {
+  num.addEventListener('click', function () {
+    // display.textContent = num.id;
+    uneseniBrojevi.push(num.id);
+    userTyping = true;
+    concatNum();
+    display.textContent = spojeniBrojevi;
+  });
+});
+
+// if ((userTyping = false)) {
+//   spojeniBrojevi = '';
+// }
+
+//pohrana unesenih brojeva u array
+const uneseniBrojevi = [];
+const odabranaOperacija = [];
+let spojeniBrojevi = '';
+
+document.querySelector('.jednako').addEventListener('click', function () {
+  console.log(uneseniBrojevi);
+  userTyping = false;
+  uneseniBrojevi.length = 0;
+  // display.textContent = `=${spojeniBrojevi}`;
+  let total = minus();
+  display.textContent = `=${total}`;
+});
+
+const operatori = document.querySelectorAll('.operator');
+operatori.forEach((funkcija) => {
+  funkcija.addEventListener('click', function () {
+    odabranaOperacija.push(funkcija.id);
+    userTyping = false;
+    uneseniBrojevi.length = 0;
+    // spojeniBrojevi = '';
+    display.textContent = '';
+  });
+});
+
+//očisti array kada stisnem C
+document.querySelector('.clear').addEventListener('click', function () {
+  uneseniBrojevi.length = 0;
+  spojeniBrojevi = '';
+  display.textContent = '';
+});
+
+const concatNum = function () {
+  if ((userTyping = true)) {
+    spojeniBrojevi = uneseniBrojevi.join('');
+  }
+};
+
+console.log(spojeniBrojevi);
+
+document.querySelector('.minus').addEventListener('click', function () {
+  // let total = minus();
+  // display.textContent = total;
+  spojeniBrojevi = '';
+});
